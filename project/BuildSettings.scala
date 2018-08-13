15a66999d6d2e499a7f18ab31211e208cf28cf54
andrius <andrius.aucinas@gmail.com>
2018-08-13 10:46:50 +0100

    publish 0.0.11 version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -18,7 +18,7 @@ object BasicSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "org.hatdex",
-    version := "0.0.11-SNAPSHOT",
+    version := "0.0.11",
     resolvers ++= Dependencies.resolvers,
     scalaVersion := Dependencies.Versions.scalaVersion,
     crossScalaVersions := Dependencies.Versions.crossScala,
